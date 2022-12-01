c883d9c1416b80d5b0d667fba0550f603781d52e
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2022-12-01 15:20:34 -0400

    55

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { BlogPage } from "./Components/BlogPage";
+import { HomePage } from "./Components/HomePage";
+import { Menu } from "./Components/Menu";
+import { NotFoundPage } from "./Components/NotFoundPage";
+import { ProfilePage } from "./Components/ProfilePage";
+function App() {
+
+
+  return (
+  <>
+  <HashRouter>
+    <Menu/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+      <Route path="/blog" element={<BlogPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
+  </HashRouter>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Components/Menu/index.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export const Menu = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          {routes.map(({ to, text }) => (
+            <li>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+                to={to}
+              >
+                {text}
+              </NavLink>
+            </li>
+          ))}
+          {/** 
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='/blog'>Blog</Link>
+          </li>
+
+        */}
+          {/**<li>
+            <NavLink
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+              to="/profile"
+            >
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+          </li>*/}
+        </ul>
+      </nav>
+    </div>
+  );
+
+
+};
+const routes = [];
+routes.push({
+  to: "/",
+  text: "Home",
+});
+routes.push({
+  to: "/profile",
+  text: "Profile",
+});
+routes.push({
+  to: "/blog",
+  text: "Blog",
+});
